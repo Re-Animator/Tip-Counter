c7c5b67de69ea32a959d12efdd3cd3ba0dccb60b
Re-Animator <reanimator1904@gmail.com>
2023-02-21 06:44:31 +0400

    theme change bug fixes

--- app/src/main/java/com/renimator/tipapp/MainActivity.kt
+++ app/src/main/java/com/renimator/tipapp/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.renimator.tipapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
-import androidx.core.view.isVisible
 import com.renimator.tipapp.databinding.ActivityMainBinding
 import java.text.NumberFormat
 import kotlin.math.ceil
@@ -17,7 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     private var groupState = "gone"
 
-    lateinit private var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.splitSwitch.setOnClickListener {
-            if(groupState == GONE) visibilityChange(View.VISIBLE, VISIBLE)
+            if (groupState == GONE) visibilityChange(View.VISIBLE, VISIBLE)
             else visibilityChange(View.GONE, GONE)
         }
     }
@@ -46,62 +45,80 @@ class MainActivity : AppCompatActivity() {
      */
     private fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
-        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val cost = stringInTextField.toDoubleOrNull()
 
-        val tipPercentage = when(selectedId) {
+        if (cost == null || cost == 0.0) {
+            displayTip(0.0)
+            return
+        }
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
 
         var tip = cost * tipPercentage
-        if(binding.roundUpSwitch.isChecked) {
+        if (binding.roundUpSwitch.isChecked) {
             tip = ceil(tip)
         }
 
+        displayTip(tip)
+    }
+
+    private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 
+
     /**
 
 
-        function for showing tip for service +++++ how much money should spend each person in a group
-            including counted tip  (need further code refactoring)
+    function for showing tip for service +++++ how much money should spend each person in a group
+    including counted tip  (need further code refactoring)
 
      */
 
     private fun calculateTip2() {
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
+        val cost = stringInTextField.toDoubleOrNull()
 
-        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId) {
+        if (cost == null || cost == 0.0) {
+            binding.tipResult.text = ""
+            return
+        }
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
 
-        var tip = cost * tipPercentage
+        val tip = cost * tipPercentage
 
-        if(binding.roundUpSwitch.isChecked) {
-            val formattedTip = NumberFormat.getCurrencyInstance().format(ceil(tip))
-            binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
-        } else {
-            val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-            binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
-        }
+        val formattedTip = if (binding.roundUpSwitch.isChecked) NumberFormat.getCurrencyInstance()
+            .format(ceil(tip))
+        else NumberFormat.getCurrencyInstance().format(tip)
 
+//        if(binding.roundUpSwitch.isChecked) {
+//            val formattedTip = NumberFormat.getCurrencyInstance().format(ceil(tip))
+//            binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+//        } else {
+//            val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+//            binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+//        }
 
-        Log.d("CheckData", "${binding.groupSize.text.toString() != null}")
-        Log.d("CheckData", binding.groupSize.text.toString())
-        if(groupState == VISIBLE && !binding.groupSize.text.toString().equals("")) {
-            val groupSize: Double = binding.groupSize.text.toString().toDouble()
-                Log.d("CheckData", groupSize.toString())
-                val personalExpenses = NumberFormat.getCurrencyInstance().format((tip + cost) / groupSize)
-                binding.costPerPerson.text = getString(R.string.cost_per_person, personalExpenses)
-        } else {
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+
+        if (groupState == VISIBLE && binding.groupSize.text.toString() != "") {
+            val groupSize = binding.groupSize.text.toString().toDouble()
+            val personalExpenses =
+                NumberFormat.getCurrencyInstance().format((tip + cost) / groupSize)
+            binding.costPerPerson.text = getString(R.string.cost_per_person, personalExpenses)
+        } else if (groupState == GONE) {
             binding.costPerPerson.text = ""
+        } else {
             Toast.makeText(this, "Fill in the group size field", Toast.LENGTH_SHORT).show()
         }
     }
